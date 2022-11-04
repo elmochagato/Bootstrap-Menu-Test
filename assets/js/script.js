b144b104509903dd1254923c9418f3cb4dd9e541
elmochagato <ozgucapb45@gmail.com>
2022-11-04 10:43:18 +0300

    big changes - update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,47 @@
-const buttons = document.querySelectorAll("button");
+
+
+(function() {
+  "use strict";
+
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+ let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -13,3 +56,5 @@ buttons.forEach((button) => {
     }
   });
 });
+
+})()
