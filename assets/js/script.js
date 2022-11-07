9499f38af96e8f5f5a342c0a3534e4421494d5c2
elmochagato <ozgucapb45@gmail.com>
2022-11-07 05:12:15 +0300

    update vx

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,32 @@
     });
   });
 
+  window.addEventListener('load', () => {
+    let menuContainer = select('.menu-container');
+    if (menuContainer) {
+      let menuIsotope = new Isotope(menuContainer, {
+        itemSelector: '.menu-item',
+        layoutMode: 'fitRows'
+      });
+
+      let menuFilters = select('#menu-filters li', true);
+
+      on('click', '#menu-filters li', function(e) {
+        e.preventDefault();
+        menuFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        menuIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+
+  });
+
   let CarouselIndicators = select("#fade-carousel-indicators");
   let carouselExampleFadeItems = select(
     "#carouselExampleFade .carousel-item",
